117e4653d7134970f1fad29ac8c95a1aa34859ca
Joel Friedman and Mark Nicolosi <asher.friedman@gmail.com>
2016-09-07 08:40:24 -0500

    1.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.aclaimant/paper-pusher "1.2.0"
+(defproject com.aclaimant/paper-pusher "1.2.1-SNAPSHOT"
   :description "Fill fillable PDFs."
   :url "https://github.com/aclaimant/paper-pusher"
   :license {:name "AGPL"
