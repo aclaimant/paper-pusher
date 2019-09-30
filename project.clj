a2d5535a2e08ff2d813230faf9a5f0310cc8a948
Mark Nicolosi <mark@aclaimant.com>
2019-09-30 10:20:33 -0500

    Let's keep trucking (update more versions of things)

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[com.itextpdf/itextpdf "5.5.13"]
                  [com.outpace/config "0.9.0"]
-                 [compojure "1.5.0"]
+                 [compojure "1.6.1"]
                  [fogus/ring-edn "0.3.0"]
                  [org.clojure/clojure "1.8.0"]
-                 [ring "1.2.1"]
+                 [ring "1.7.1"]
                  [org.bouncycastle/bcprov-jdk15on "1.55"]
                  [org.bouncycastle/bcmail-jdk15on "1.55"]]
   :plugins [[lein-ring "0.12.5"]]
